4fda8a5225a7754803b5d3c98f5aee5402b3e1fe
Branislav Vaclav <brano.vaclav@gmail.com>
2017-09-25 15:47:59 +0200

    revert platform dependenet fix try

--- server.js
+++ server.js
@@ -11,11 +11,7 @@ const host = process.env.HOST || '127.0.0.1';
 const port = process.env.PORT || 3000;
 
 const root = __dirname;
-// const imgpath = path.join(root, 'data', 'images');
-const paths = {
-	images: path.join('data', 'images', '/'),
-	thumbnails: path.join('data', 'images', 'thumbnails', '/')
-}
+const imgpath = path.join(root, 'data', 'images');
 
 let lang = 'sk';
 let data = {};
@@ -38,14 +34,14 @@ const db = new loki('data/db.js', {
 
 		// data.pois.sorted = data.pois.collection.chain().sort(abcsort).data();
 		data.pois.all.forEach(poi => {
-			try { poi.files = fs.readdirSync(path.join(paths.images, poi.folder)).filter(file => fs.lstatSync(path.join(paths.images, poi.folder, file)).isFile()).map(file => path.join(poi.folder, file)); }
+			try { poi.files = fs.readdirSync(path.join(imgpath, poi.folder)).filter(file => fs.lstatSync(path.join(imgpath, poi.folder, file)).isFile()).map(file => `${ poi.folder }/${ file }`); }
 			catch (err) { poi.files = []; }
 		});
 	}
 });
 
 for (let region in constants.regions) {
-	try { constants.regions[region].files = fs.readdirSync(path.join(paths.images, region)).filter(file => fs.lstatSync(path.join(paths.images, region, file)).isFile()).map(file => path.join(region, file)); }
+	try { constants.regions[region].files = fs.readdirSync(path.join(imgpath, region)).filter(file => fs.lstatSync(path.join(imgpath, region, file)).isFile()).map(file => `${ region }/${ file }`); }
 	catch (err) { constants.regions[region].files = []; }
 }
 
@@ -57,6 +53,10 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(root, 'src', 'views'));
 
 //app
+const paths = {
+	images: 'data/images/',
+	thumbnails: 'data/images/thumbnails/'
+}
 app.get('/data.js', (req, res) => {
 	res.render('data.ejs', { pois: data.pois.all, articles: data.articles.all, maps: constants.maps, regions: constants.regions, paths, lang });
 });
